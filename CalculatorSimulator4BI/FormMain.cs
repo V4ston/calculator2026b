501d976a8681db1f7a395c965a1e437140df9c87
Valentin Atanasov <v.atanasov.3866@vallauri.edu>
2026-09-16 11:16:17 +0200

    creazione dinamica bottoni semplificata

--- CalculatorSimulator4BI/FormMain.cs
+++ CalculatorSimulator4BI/FormMain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,16 @@ namespace CalculatorSimulator4BI
 {
     public partial class FormMain : Form
     {
+        private char[,] buttons =
+        {
+            { '%', '\u0152', 'C', '\u232B'},
+            { '\u215F', '\u00B2', '\u221A', '\u00F7'},
+            { '7', '8', '9', 'x'},
+            { '4', '5', '6', '-'},
+            { '1', '2', '3', '+'},
+            { '\u00B1', '0', ',', '='}
+        };
+
         public FormMain()
         {
             InitializeComponent();
@@ -19,7 +30,37 @@ namespace CalculatorSimulator4BI
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            MakeButtons();
+        }
 
+        private void MakeButtons()
+        {
+            int btnWidth = 80;
+            int btnHeigth = 60;
+            int posY = 106;
+
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+
+                int posX = 0;
+
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+                    Button btn = new Button();
+                    btn.Width = btnWidth;
+                    btn.Height = btnHeigth;
+                    btn.Top = posY;
+                    btn.Left = posX;
+                    btn.Font = new Font("Segoe UI", 16);
+                    btn.Text = buttons[i, j].ToString();
+                    btn.ForeColor = Color.Black;
+
+                    Controls.Add(btn);
+
+                    posX += btnWidth;
+                }
+                posY += btnHeigth;
+            }
         }
     }
 }
